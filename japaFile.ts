48f2c35508c6e8912f75761bc1821c031e1e5904
salsifitewn <salsifite.gmail.com>
2021-07-14 20:46:30 +0200

    :wrench: update test config

--- japaFile.ts
+++ japaFile.ts
@@ -31,7 +31,7 @@ async function startHttpServer() {
  * Configure test runner
  */
 configure({
-  files: ['test/**/*.spec.ts'],
+  files: ['**/*.spec.ts'],
   before: [runMigrations, startHttpServer],
   after: [rollbackMigrations],
 })
